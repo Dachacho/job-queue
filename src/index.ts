664851db86d9971f81783ac014eaa59d38ff3a0c
Dachacho <arevadzedachi123@gmail.com>
2025-11-03 00:41:16 +0400

    add basic post for jobs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,56 @@
 import express from "express";
+import { Request, Response } from "express";
+import { randomUUID } from "crypto";
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
+export interface Job {
+  jobId: string;
+  jobType: string;
+  jobData: string;
+  jobPriority: number;
+}
+
+const queue: Job[] = [];
 
 app.get("/health", (_req, res) => res.json({ status: "ok" }));
 
 app.get("/", (_req, res) => {
   res.send("Hello from server");
+});
+
+app.post("/jobs", (req: Request, res: Response) => {
+  try {
+    const { type, data, priority } = req.body;
+    let defaultPriority = -1;
+
+    if (!type || !data) {
+      return res.status(400).json({ message: "fields mising" });
+    }
+
+    if (priority) {
+      defaultPriority = priority;
+    }
+
+    const id = randomUUID();
+
+    let job: Job = {
+      jobId: id,
+      jobType: type,
+      jobData: data,
+      jobPriority: defaultPriority,
+    };
+
+    queue.push(job);
+
+    console.log(queue);
+  } catch (err) {
+    console.log("error", err);
+    return res.json({ message: (err as Error).message });
+  }
 });
 
 app.listen(port, () => {
